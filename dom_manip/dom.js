0e58c5cab8eedfb19868f03fc5a0abe43d924fb5
kishanms999 <kishanms99@gmail.com>
2023-03-14 17:29:38 +0530

    third commit used getelementsbytagname

--- dom_manip/dom.js
+++ dom_manip/dom.js
@@ -1,17 +1,29 @@
 // console.log(document.getElementById('header-title'));
-var headerTitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
+// var headerTitle=document.getElementById('header-title');
+// var header=document.getElementById('main-header');
 
-// headerTitle.textContent='Hello';
-// headerTitle.innerHTML=<h3>Hello</h3>;
-header.style.borderBottom='solid 3px #000';
-console.log(document.getElementsByClassName('title'));
-var newTitle=document.getElementsByClassName('title');
-newTitle[0].style.fontWeight='bold';
-newTitle[0].style.color='green';
-var items=document.getElementsByClassName('list-group-item');
-items[1].textContent='Hello 2';
-items[2].style.backgroundColor='green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
+// // headerTitle.textContent='Hello';
+// // headerTitle.innerHTML=<h3>Hello</h3>;
+// header.style.borderBottom='solid 3px #000';
+// console.log(document.getElementsByClassName('title'));
+// var newTitle=document.getElementsByClassName('title');
+// newTitle[0].style.fontWeight='bold';
+// newTitle[0].style.color='green';
+// var items=document.getElementsByClassName('list-group-item');
+// items[1].textContent='Hello 2';
+// items[2].style.backgroundColor='green';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+
+// header.style.borderBottom='solid 3px #000';
+// console.log(document.getElementsByClassName('title'));
+// var newTitle=document.getElementsByClassName('title');
+// newTitle[0].style.fontWeight='bold';
+// newTitle[0].style.color='green';
+var li=document.getElementsByTagName('li');
+li[1].textContent='Hello 2';
+li[2].style.backgroundColor='green';
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
 }
