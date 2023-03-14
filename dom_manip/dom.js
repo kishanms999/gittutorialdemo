17259a07006354d37e0779943b816f13b5ebd599
kishanms999 <kishanms99@gmail.com>
2023-03-14 15:50:44 +0530

    third commit using getelementsbyclassname

--- dom_manip/dom.js
+++ dom_manip/dom.js
@@ -9,3 +9,9 @@ console.log(document.getElementsByClassName('title'));
 var newTitle=document.getElementsByClassName('title');
 newTitle[0].style.fontWeight='bold';
 newTitle[0].style.color='green';
+var items=document.getElementsByClassName('list-group-item');
+items[1].textContent='Hello 2';
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
